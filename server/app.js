eacd41e931bdbf6b7a699036de4d7175b4a39f11
Garrit Franke <garritfranke@gmail.com>
2020-10-05 21:14:05 +0200

    server: customers: add GET and POST

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -6,6 +7,14 @@ app.use(require("body-parser").json());
 
 const cors = require("cors")({ origin: true });
 app.use(cors);
+
+mongoose
+  .connect("mongodb://root:example@localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected"))
+  .catch(console.error);
 
 app.use("/customers", require("./routes/customers"));
 
--- server/routes/customers.js
+++ server/routes/customers.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Hi from customers");
+const Customer = require("../model/Customer");
+
+router.get("/", async (req, res) => {
+  const customer = await Customer.find();
+  res.json(customer);
+});
+
+router.post("/", async (req, res) => {
+  console.log("POSTing customer");
+  const { name } = req.body;
+
+  const customer = new Customer({ name });
+  const savedCustomer = await customer.save();
+  res.json(savedCustomer);
 });
 
 module.exports = router;
