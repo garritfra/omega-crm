aed3c563c7a3120e59c22ebe0f6c2a529c82688a
Garrit Franke <garritfranke@gmail.com>
2020-10-17 23:56:43 +0200

    Migrate services

---
+++ client/omega/service/ClientService.ts
@@ -0,0 +1,35 @@
+import axios from "./api-client";
+
+const basepath = process.env.API_BASE_PATH;
+
+export interface Client {
+  id: string;
+  name: string;
+}
+
+export interface NewClient {
+  name: string;
+}
+
+export default {
+  getClients(): Promise<Client[]> {
+    return axios
+      .get(basepath + "/clients")
+      .then((res) => res.data)
+      .then((data) =>
+        data.map((client) => {
+          return { id: client._id, name: client.name };
+        })
+      );
+  },
+
+  addClient(client: NewClient): Promise<Client> {
+    return axios
+      .post(basepath + "/clients", client)
+      .then((res) => res.data)
+      .then((client) => {
+        return { ...client, id: client._id };
+      })
+      .then((data) => data as Client);
+  },
+};
