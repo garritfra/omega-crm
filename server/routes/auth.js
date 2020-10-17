e886fc3c083c2808918a160af42c24b2c834d2a7
Garrit Franke <garritfranke@gmail.com>
2020-10-17 19:35:48 +0200

    Add debug output to auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,7 @@ const User = require("../model/User");
 
 router.post("/register", async (req, res) => {
   const { email, full_name, password } = req.body;
+  console.debug("Registering user:", email)
 
   if (await User.findOne({ email })) {
     res.status(400).send("Email already taken");
@@ -23,26 +24,31 @@ router.post("/register", async (req, res) => {
       user.password = undefined;
       res.status(201).json(user);
     })
-    .catch((err) => res.status(400).json(err));
+    .catch((err) => { console.log(err); res.status(400).json(err) });
 });
 
 router.post("/login", async (req, res) => {
+  console.debug("New login attempt:", req.body.email)
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
 
-  if (!user) return res.status(400).send("Email or password incorrect");
+  if (!user) {
+    console.debug("User not found")
+    return res.status(400).send("Email or password incorrect");
+  }
 
   const validPass = await bcrypt.compare(password, user.password);
   if (!validPass) return res.status(400).send("Email or password incorrect");
 
   const token = jwt.sign({ _id: user.id }, process.env.AUTH_SECRET);
 
+  console.debug("Login success for userId:", user.id)
   res.json({ status: "success", token, id: user.id });
 });
 
 router.get("/profile", async (req, res) => {
-  console.debug(req.userId);
+  console.debug("Fetching profile for:", req.userId);
   let user = await User.findOne({ _id: req.userId });
   user.password = undefined;
   return res.json(user);
