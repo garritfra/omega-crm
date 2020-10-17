a2441019ff1f356b2e2d753502621b13b8d36aac
Garrit Franke <garritfranke@gmail.com>
2020-10-17 23:58:09 +0200

    Migrate clients page

---
+++ client/omega/pages/clients.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { List, Avatar, Button } from "antd";
+import { UserAddOutlined, UserOutlined } from "@ant-design/icons";
+import ClientService from "../service/ClientService";
+
+export default function ClientsPage() {
+  const [clients, setClients] = useState([]);
+  useEffect(() => {
+    ClientService.getClients().then(setClients);
+  }, []);
+
+  return (
+    <div>
+      <List
+        dataSource={clients}
+        itemLayout="horizontal"
+        header={
+          <>
+            <h2>Clients</h2>
+            <Button
+              type="primary"
+              href="/clients/new"
+              icon={<UserAddOutlined />}
+            >
+              New
+            </Button>
+          </>
+        }
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar icon={<UserOutlined />} />}
+              title={<a href={"/clients/" + item.id}>{item.name}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            />
+          </List.Item>
+        )}
+      ></List>
+    </div>
+  );
+}
