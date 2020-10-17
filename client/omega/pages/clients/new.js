08bda14708108afa446c1ec0043d95eca63b4f50
Garrit Franke <garritfranke@gmail.com>
2020-10-17 23:59:36 +0200

    migrate /clients/new

---
+++ client/omega/pages/clients/new.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Form, Input, Button } from "antd";
+import ClientService from "../../service/ClientService";
+
+export default function NewClientPage() {
+  const onSubmit = (values) => {
+    ClientService.addClient(values).then(
+      (client) => (window.location = "/clients/" + client.id)
+    );
+  };
+
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 4 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 4 },
+  };
+
+  return (
+    <Form onFinish={onSubmit} {...layout}>
+      <Form.Item label="Name" name="name">
+        <Input></Input>
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
