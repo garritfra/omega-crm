278fb075918de16ac1ff8a284794f527ac1eeb02
Garrit Franke <garritfranke@gmail.com>
2020-10-18 20:33:16 +0200

    Adjust client info layout

--- client/src/pages/ClientPage.js
+++ client/src/pages/ClientPage.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Descriptions, Timeline, Tag, Space, Layout } from "antd";
+import {
+  Descriptions,
+  Timeline,
+  Tag,
+  Space,
+  List,
+  Typography,
+  Statistic,
+  Row,
+  Col,
+} from "antd";
 import ClientService from "../service/ClientService";
 
 export default function ClientPage({ id }) {
@@ -11,30 +21,45 @@ export default function ClientPage({ id }) {
   return (
     <>
       <Space direction="vertical" size="large">
-        <Descriptions title="Client Info" bordered>
-          <Descriptions.Item label="Name">{client.name}</Descriptions.Item>
-          <Descriptions.Item label="Identifier">{client.id}</Descriptions.Item>
-          <Descriptions.Item label="Status">
-            <Tag color="error">Inactive</Tag>
-          </Descriptions.Item>
-        </Descriptions>
-        <Timeline mode="left">
-          <Timeline.Item label="04.10.2020">
-            Status changed: <Tag color="error">Inactive</Tag>
-          </Timeline.Item>
-          <Timeline.Item label="03.10.2020">
-            Status changed: <Tag color="purple">On Hold</Tag>
-          </Timeline.Item>
-          <Timeline.Item label="03.10.2020">
-            Project added: Foo Bar
-          </Timeline.Item>
-          <Timeline.Item label="02.10.2020">
-            Status changed: <Tag color="success">Active</Tag>
-          </Timeline.Item>
-          <Timeline.Item label="01.10.2020">
-            Status changed: <Tag color="default">Potential</Tag>
-          </Timeline.Item>
-        </Timeline>
+        <Typography.Title level={4}>Client Info</Typography.Title>
+        <Row gutter={16}>
+          <Col>
+            <List bordered split="true">
+              <List.Item actions={[client.name]}>Name:</List.Item>
+              <List.Item actions={[client.id]}>Identifier:</List.Item>
+              <List.Item actions={[<Tag color="error">Inactive</Tag>]}>
+                Status:
+              </List.Item>
+            </List>
+          </Col>
+          <Col>
+            <Statistic title="Total Billed" value={3125} suffix="€" />
+            <Statistic title="Total payed" value={2400} suffix="€" />
+            <Statistic title="Total Balance" value={3125 - 2400} suffix="€" />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={16}>
+            <Timeline mode="left">
+              <Timeline.Item label="04.10.2020">
+                Status changed: <Tag color="error">Inactive</Tag>
+              </Timeline.Item>
+              <Timeline.Item label="03.10.2020">
+                Status changed: <Tag color="purple">On Hold</Tag>
+              </Timeline.Item>
+              <Timeline.Item label="03.10.2020">
+                Project added: Foo Bar
+              </Timeline.Item>
+              <Timeline.Item label="02.10.2020">
+                Status changed: <Tag color="success">Active</Tag>
+              </Timeline.Item>
+              <Timeline.Item label="01.10.2020">
+                Status changed: <Tag color="default">Potential</Tag>
+              </Timeline.Item>
+            </Timeline>
+          </Col>
+          <Col span={8}></Col>
+        </Row>
       </Space>
     </>
   );
