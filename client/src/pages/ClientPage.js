c4528bedb65e8cfbef10e0b08f935fe52a8e01a2
Garrit Franke <garritfranke@gmail.com>
2020-10-18 15:51:35 +0200

    client: add clients description and timeline

--- client/src/pages/ClientPage.js
+++ client/src/pages/ClientPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Descriptions, Timeline, Tag, Space, Layout } from "antd";
 import ClientService from "../service/ClientService";
 
 export default function ClientPage({ id }) {
@@ -7,5 +8,34 @@ export default function ClientPage({ id }) {
     ClientService.getClientById(id).then(setClient);
   }, []);
 
-  return <div>{client.name}</div>;
+  return (
+    <>
+      <Space direction="vertical" size="large">
+        <Descriptions title="Client Info" bordered>
+          <Descriptions.Item label="Name">{client.name}</Descriptions.Item>
+          <Descriptions.Item label="Identifier">{client.id}</Descriptions.Item>
+          <Descriptions.Item label="Status">
+            <Tag color="error">Inactive</Tag>
+          </Descriptions.Item>
+        </Descriptions>
+        <Timeline mode="left">
+          <Timeline.Item label="04.10.2020">
+            Status changed: <Tag color="error">Inactive</Tag>
+          </Timeline.Item>
+          <Timeline.Item label="03.10.2020">
+            Status changed: <Tag color="purple">On Hold</Tag>
+          </Timeline.Item>
+          <Timeline.Item label="03.10.2020">
+            Project added: Foo Bar
+          </Timeline.Item>
+          <Timeline.Item label="02.10.2020">
+            Status changed: <Tag color="success">Active</Tag>
+          </Timeline.Item>
+          <Timeline.Item label="01.10.2020">
+            Status changed: <Tag color="default">Potential</Tag>
+          </Timeline.Item>
+        </Timeline>
+      </Space>
+    </>
+  );
 }
