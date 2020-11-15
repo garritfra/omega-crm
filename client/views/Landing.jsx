dabad5981ce2d2b49d99eb078f4b9c9145fe2079
Garrit Franke <garritfranke@gmail.com>
2020-11-15 23:11:59 +0100

    Style Landing page

--- client/views/Landing.jsx
+++ client/views/Landing.jsx
@@ -4,8 +4,27 @@ import Layout from "./layouts/Main";
 export default function Index() {
   return (
     <Layout>
-      <a href="/auth/login">Login</a>
-      <a href="/auth/register">Register</a>
+      <div className="jumbotron">
+        <h1 className="display-4">Ωmega</h1>
+        <p class="lead">The last CRM you will ever need.</p>
+        <hr class="my-4"></hr>
+        <div className="d-flex flex-column">
+          <a
+            className="btn btn-primary btn-sm mb-2"
+            href="/auth/register"
+            role="button"
+          >
+            Try out the Alpha
+          </a>
+          <a
+            className="btn btn-light btn-sm mb-2"
+            href="/auth/login"
+            role="button"
+          >
+            Already have an account? Log in here
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 }
