6cc5e3dfce955d1dd83685304bf089833aad9d81
Garrit Franke <garritfranke@gmail.com>
2020-11-15 22:19:53 +0100

    Add dashboard jumbotron

--- client/views/Index.jsx
+++ client/views/Index.jsx
@@ -4,7 +4,15 @@ import Layout from "./layouts/Main";
 export default function Index({ user }) {
   return (
     <Layout user={user}>
-      <h1>Hello</h1>
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome back, {user.name?.split(" ")[0]}!</h1>
+        <p class="lead">Thanks for testing out the Ωmega alpha.</p>
+        <hr class="my-4"></hr>
+        <p>Features currently include:</p>
+        <a class="btn btn-primary btn-sm" href="/clients" role="button">
+          Manage Clients
+        </a>
+      </div>
     </Layout>
   );
 }
